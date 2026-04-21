1ec076576c0d0cbd298b7da525bf216e089c49df
Eliana Fernandez <elianafernandez916@gmail.com>
2026-04-20 23:38:55 -0300

    Agrege Menu

--- src/main/java/app/Program.java
+++ src/main/java/app/Program.java
@@ -1,13 +1,14 @@
 package app;
+import views.ListarVehiculosView;
 
 import data.Persistencia;
 import java.util.InvalidPropertiesFormatException;
-import views.ListarVehiculosView;
+import views.Menu;
 
 public class Program {
     public static void main(String[] args) throws IllegalArgumentException, InvalidPropertiesFormatException {
         Persistencia.inicializar();
-        ListarVehiculosView view = new ListarVehiculosView();
-        view.setVisible(true);
+
+        new Menu().setVisible(true);
     }
 }
