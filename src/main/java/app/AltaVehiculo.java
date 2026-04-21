3866dc966cda32c5150822e12b4ef0e319af6ea9
Ana Carolina Salazar <Ana.salazar@alu.frt.utn.edu.ar>
2026-04-20 23:03:16 -0300

    Actualización Alta Vehiculo

--- src/main/java/app/AltaVehiculo.java
+++ src/main/java/app/AltaVehiculo.java
@@ -4,10 +4,52 @@
  */
 package app;
 
-/**
- *
- * @author csala
- */
+import java.util.Scanner;
+import domain.*;
+
 public class AltaVehiculo {
-    
+
+    public static Vehiculo crearVehiculo() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese marca: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Ingrese país: ");
+        String pais = sc.nextLine();
+
+        Marca marca = new Marca(nombre, pais);
+
+        System.out.print("Ingrese patente: ");
+        String patente = sc.nextLine();
+
+        System.out.print("Ingrese modelo: ");
+        String modelo = sc.nextLine();
+
+        System.out.print("Ingrese año: ");
+        int anio = sc.nextInt();
+
+        System.out.print("Capacidad de carga: ");
+        double capacidad = sc.nextDouble();
+
+        System.out.print("Km por litro: ");
+        double kmLitro = sc.nextDouble();
+
+        System.out.print("Litros extra: ");
+        double litrosExtra = sc.nextDouble();
+
+        sc.nextLine();
+
+        return new VehiculoCombustible(
+                patente,
+                marca.getNombre(),
+                modelo,
+                anio,
+                capacidad,
+                null,
+                kmLitro,
+                litrosExtra
+        );
+    }
 }
